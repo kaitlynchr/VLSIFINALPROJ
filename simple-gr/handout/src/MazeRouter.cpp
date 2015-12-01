8dc2056a844a90e52c773ebe9bcb9d17cda46973
kaitlynchr <kaitlynchristensen@gmail.com>
2015-11-30 17:15:17 -0700

    commit one

---
+++ simple-gr/handout/src/MazeRouter.cpp
@@ -0,0 +1,81 @@
+/*
+ * MazeRouter.cpp
+ * Work on this file to complete your maze router
+ */
+
+using namespace std;
+
+#include "SimpleGR.h"
+
+///////////////////////////////////////////////////////////////////////////////
+// Implement an A* search based maze routing algorithm
+// and a corresponding back-trace procedure
+// The function needs to correctly deal with the following conditions:
+// 1. Only search within a bounding box defined by botleft and topright points
+// 2. Control if any overflow on the path is allowed or not
+///////////////////////////////////////////////////////////////////////////////
+CostType SimpleGR::routeMaze(Net& net, bool allowOverflow, const Point &botleft,
+                             const Point &topright, const EdgeCost &func, vector<Edge*> &path)
+{
+  // find out the ID of the source and sink gcells
+  const IdType srcGCellId = getGCellId(net.gCellOne);
+  const IdType snkGCellId = getGCellId(net.gCellTwo);
+
+  // insert the source gcell into the priority queue
+  priorityQueue.setGCellCost(srcGCellId, 0., 0., NULLID);
+
+  // Instantiate the Cost function to calculate the Manhattan distance between
+  // two gcells. This distance is used as the heuristic cost for A* search.
+  ManhattanCost &lb = ManhattanCost::getFunc();
+//A*(const Point src, const Point snk)
+{
+Gcell GCELL; 
+  // A* search kernel
+  // Loop until all "frontiers" in the priority queue are exhausted, or when
+  // the sink gcell is found.
+  do{ 
+    // YOUR A* search CODE GOES IN HERE
+	GCELL = getGCell(priorityQueue.getBestGCell()); 
+	if((snk ==  GCELL){
+   		break;} //got to get out of the loop somehow
+	priorityQueue.rmBestGCell(); 
+	
+	if(GCELL.incx != NULLID)
+	{ grEdgeArr[GCELL.incx]; 
+		if(getGCellId(Edge.gcell1) == getGCellId(GCELL))
+		{
+			 if(!isGCellVsted(EDGEINCX.gcell2))
+			{
+				const GCellData& Data= getGCellData(getGCellId(GCELL));
+				priorityQueue.setGCellCost(getGCellId(EDGEINCX.gcell2,Data.pathCost+func(EDGEINCX.id)+lb(snk,EDGEINCX.gcell2) , Data.pathCost+func(EDGEINCX.id), getGCellId(GCELL))  
+			}
+		}
+	}
+
+	
+
+
+
+    // YOUR A* search CODE ENDS HERE
+  } while (!priorityQueue.isEmpty());
+//current= node in openset having the lowest f_score[]
+
+  // now backtrace and build up the path, if we found one
+  // back-track from sink to source, and fill up 'path' vector with all the edges that are traversed
+  if (priorityQueue.isGCellVsted(snkGCellId)) {
+    // YOUR backtrace CODE GOES IN HERE
+	path.push(EDGE 	);	
+    // YOUR backtrace CODE ENDS HERE
+  }
+  // calculate the accumulated cost of the path
+  const CostType finalCost = 
+      priorityQueue.isGCellVsted(snkGCellId) ?
+          priorityQueue.getGCellData(snkGCellId).pathCost : numeric_limits<CostType>::max();
+
+  // clean up
+  priorityQueue.clear();
+
+  return finalCost;
+}
+
+
---
+++ simple-gr/handout/src/README.txt
@@ -0,0 +1,10 @@
+Kaitlyn Christensen
+A01284997 
+
+What is the purpose of each of the 3 stages in this routing alogrithm? How are they different from each other? How do they work together as flow?
+The purpose of the 3 stages first to ensure that a solution is found, and then to ensure the best solution is found.  The first stage is just to get it routed.  The next part is to find the problems and blockages and fix it so that
+it can be routed. It is also to take wires that are in the way of other wires and replace them. The final is to check and find the optimal route of the wires. 
+
+How does SimpleGr build a framework to allow the 3 stages?
+It starts out with the intital routing, then it does the RRR, and it ends with greedy to ensure the least amount of wires
+
