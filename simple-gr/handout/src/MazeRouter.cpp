29416b114f48dc919f6503d2d56816af3793e97d
kaitlynchr <kaitlynchristensen@gmail.com>
2015-12-01 22:41:38 -0700

    HEH

--- simple-gr/handout/src/MazeRouter.cpp
+++ simple-gr/handout/src/MazeRouter.cpp
@@ -20,7 +20,7 @@ CostType SimpleGR::routeMaze(Net& net, bool allowOverflow, const Point &botleft,
   // find out the ID of the source and sink gcells
   const IdType srcGCellId = getGCellId(net.gCellOne);
   const IdType snkGCellId = getGCellId(net.gCellTwo);
-
+cout<<"SINKCELLID"<<snkGCellId<<endl<<endl;
   // insert the source gcell into the priority queue
   priorityQueue.setGCellCost(srcGCellId, 0., 0., NULLID);
 
@@ -34,7 +34,8 @@ GCell GCELL, snk=getGCell(snkGCellId);
 do{ 
     // YOUR A* search CODE GOES IN HERE
     //start off with the best GCELL to help decide where to go next
-	GCELL = getGCell(priorityQueue.getBestGCell()); 
+	GCELL = getGCell(priorityQueue.getBestGCell());
+	cout<<"GCELLID"<<getGCellId(GCELL)<<endl; 
 	//here we check to see if we have reaced our end result, if so we get out of the loop.
 	if(snkGCellId ==  getGCellId(GCELL)){
    		break;} //got to get out of the loop somehow
@@ -42,8 +43,10 @@ do{
 	priorityQueue.rmBestGCell(); 
 	// get the edge Id to get the next GCELL values to check for
 	int checkValues=0; 
-	IdType EDGEID=GCELL.incX, LOOP[6];
-	checkValues++; 
+	IdType EDGEID, LOOP[6];
+	checkValues++;
+//	cout<<endl<<endl<<numLayers<<endl<<endl; 
+//std::cout<<"hello 1\n";
 	if(GCELL.x < gcellArrSzY-1){
 	 LOOP[checkValues] = GCELL.incX;
 	checkValues++;}
@@ -62,19 +65,26 @@ do{
 	if(GCELL.y > 0) 
 	{ LOOP[checkValues] = GCELL.decZ;
 	checkValues++;} 
-	
+//std::cout<<"hello 2\n";
+	Edge *Pedge;
 	//make sure it exisits
 	for(int i=0; i < checkValues; i++)
 	{
 		EDGEID=LOOP[i];
 		if(EDGEID != NULLID )
 		{
+//std::cout<<"hello 3\n";
+		Pedge = &grEdgeArr[EDGEID];  
 			//puts GCELL1 and GCELL2 in their own GCELL to make cooding easier 
-		GCell GCELL1 = *grEdgeArr[EDGEID].gcell1;
-		GCell GCELL2 = *grEdgeArr[EDGEID].gcell2; 
-		//gets the path cost of the current GCELL to add to our calculations 
-		float pathCost = priorityQueue.getGCellData(getGCellId(GCELL)).pathCost;
+			GCell GCELL1 = *Pedge->gcell1;
+			cout<<"here";
+			//GCell GCELL2 = *Pedge->gcell2; 
+			GCell GCELL2= &grEdgeArr[EDGEID];
+			//gets the path cost of the current GCELL to add to our calculations 
+			float pathCost = priorityQueue.getGCellData(getGCellId(GCELL)).pathCost;
 			//figures out which GCELL to use (the one that is not the same as the current)
+//std::cout<<"hello 4\n";
+
 			if(getGCellId(GCELL1) == getGCellId(GCELL))
 			{
 				//checks to see if the GCELL Has been visited or is in the queue, if not it adds it 
@@ -83,19 +93,20 @@ do{
 				// gives it the ID, path cost, heuristic cost, and manhattan cost
 					priorityQueue.setGCellCost(getGCellId(GCELL2), pathCost+func(EDGEID)+lb(snk,GCELL2) ,pathCost+func(EDGEID), getGCellId(GCELL));  
 				}//checks to see if there is a better cost for that cell, if so it adds it
-				else if( priorityQueue.getGCellData(getGCellId(GCELL2)).totalCost > pathCost+func(EDGEID)+lb(snk,GCELL2))
+				else if( priorityQueue.getGCellData(getGCellId(GCELL2)).totalCost > pathCost+func(EDGEID)+lb(snk,GCELL2) &&priorityQueue.getGCellData(getGCellId(GCELL2)).heapLoc!= NULLID)
 				{
 					priorityQueue.setGCellCost(getGCellId(GCELL2), pathCost+func(EDGEID)+lb(snk,GCELL2) ,pathCost+func(EDGEID), getGCellId(GCELL)); 
 				}
 			}	
 			else
 			{
+
 			 	if(!priorityQueue.isGCellVsted(getGCellId(GCELL1)))
 				{
 				// gives it the ID, path cost, heuristic cost, and manhattan cost
 					priorityQueue.setGCellCost(getGCellId(GCELL1), pathCost+func(EDGEID)+lb(snk,GCELL1) ,pathCost+func(EDGEID), getGCellId(GCELL));  
 				}//checks to see if there is a better cost for that cell, if so it adds it
-				else if( priorityQueue.getGCellData(getGCellId(GCELL)).totalCost > pathCost+func(EDGEID)+lb(snk,GCELL2))
+				else if( priorityQueue.getGCellData(getGCellId(GCELL)).totalCost > pathCost+func(EDGEID)+lb(snk,GCELL2) && priorityQueue.getGCellData(getGCellId(GCELL1)).pathCost != NULLID)
 				{
 					priorityQueue.setGCellCost(getGCellId(GCELL1), pathCost+func(EDGEID)+lb(snk,GCELL1) ,pathCost+func(EDGEID), getGCellId(GCELL)); 
 				}
@@ -105,26 +116,29 @@ do{
 	}
 	// just the same as above for the next 5 sides of the GCELL, I tried doing a function but kept getting errors and it seemed longer
 	//to make the function work than to just copy and change the code for the value (only difference is the EDGEID)
-		 
-	
+	//cout<<getGCellId(GCELL)<<endl;	 
+cout<<"TEST";
     // YOUR A* search CODE ENDS HERE
   } while (!priorityQueue.isEmpty());
-
+//cout<<endl<<snkGCellId<<endl<<endl;
 GCELL=getGCell(snkGCellId);
 IdType test, EDGEID; 
 			 cout<<"here!";
 
   // now backtrace and build up the path, if we found one
   // back-track from sink to source, and fill up 'path' vector with all the edges that are traversed
-  if (priorityQueue.isGCellVsted(snkGCellId)) {    // YOUR backtrace CODE GOES IN
+//	cout<<priorityQueue.isGCellVsted(snkGCellId)<<endl; 
+ if (priorityQueue.isGCellVsted(snkGCellId)) {    // YOUR backtrace CODE GOES IN
   // creates a loop to go through all the previous cells, stops when the source is the parent
-	Edge backtrace; 
+	Edge *backtrace;
 	while(priorityQueue.getGCellData(getGCellId(GCELL)).parentGCell != srcGCellId)
 	//stores the edge so it can be pushed onto the vector
+	cout<<"rawr"<<endl;
 	test = priorityQueue.getGCellData(getGCellId(GCELL)).parentGCell;  
 	EDGEID=IDEDGE(GCELL, getGCell(test));
-	backtrace= grEdgeArr[EDGEID];
-    	path.push_back(&backtrace);
+	cout<<"pie\n";
+	backtrace= &grEdgeArr[EDGEID];
+    	path.push_back(backtrace);
     	//navigates to the next GCELL to continue its back trace
     	GCELL = getGCell(priorityQueue.getGCellData(getGCellId(GCELL)).parentGCell);
   	cout<< getGCellId(GCELL)<<endl;
@@ -133,8 +147,8 @@ IdType test, EDGEID;
 	//gets and stores the final edge  
   	test = priorityQueue.getGCellData(getGCellId(GCELL)).parentGCell;  
 	EDGEID=IDEDGE(GCELL, getGCell(test));
-	backtrace= grEdgeArr[EDGEID];
-    	path.push_back(&backtrace);
+	backtrace= &grEdgeArr[EDGEID];
+    	path.push_back(backtrace);
 }
   // calculate the accumulated cost of the path (outputs the past via snkGCELLID for total cost )
   const CostType finalCost = priorityQueue.getGCellData(snkGCellId).totalCost;
@@ -145,6 +159,6 @@ IdType test, EDGEID;
   priorityQueue.clear();
 
   return finalCost;
-}}
+}
 
 
